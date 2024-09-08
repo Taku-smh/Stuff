78753a47f3132df8651da9e2750bd1f447949e04
Taku-smh <tchikaka10@gmail.com>
2024-09-08 12:13:00 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,15 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:main/menu.dart';
 import 'package:main/recepient.dart';
 
-/*223001254 MI MTYALI
-223009523 SB TSHIMUKA
-223000453 NH MATSANE 
-223031859 K RAMOLISE
-222009992 T CHIKAKA
-223007410 T MANOSA
-223042658 A MTSHONISWA
-223026092 SS SHANGE */
-/*Question 1: Home Page */
+
 void main() {
   runApp(const MyApp());
 }
